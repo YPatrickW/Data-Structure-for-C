5802f81da7bb4aaa7a0f3b1f1578080432dd3118
patrick914y <patrick914y@gmail.com>
2020-04-26 00:37:59 +0800

    顺序栈

---
+++ 顺序栈/顺序栈/源.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#define Maxsize 100
+typedef int Elemtype;
+typedef struct stack
+{
+	Elemtype data[Maxsize];
+	int top;
+}Sqstack;
+void init_stack(Sqstack*& S)
+{
+	S = (Sqstack*)malloc(sizeof(Sqstack));
+	S->top = -1;
+}
+bool push(Sqstack* S, Elemtype e)
+{
+	if (S->top == Maxsize-1)
+	{
+		return false;
+	}
+	else
+	{
+		S->top++;
+		S->data[S->top] = e;
+		return true;
+	}
+}
+void Destory_stack(Sqstack *& S)
+{
+	free(S);
+}
+bool pop(Sqstack *S, Elemtype *e)
+{
+	if (S->top == -1)
+	{
+		return false;
+	}
+	else
+	{
+		*e = S->data[S->top];
+		S->top--;
+		return true;
+	}
+}
+bool gettop(Sqstack *S, Elemtype *e)
+{
+	if (S->top ==-1)
+	{
+		return false;
+	}
+	else
+	{
+		*e = S->data[S->top];
+		return true;
+	}
+}
+bool check_string(Elemtype str[])
+{
+	int i = 0;
+	Sqstack*S;
+	Elemtype *e;
+	init_stack(S);
+	for ( i = 0; str[i]!= '\0' ; i++)
+	{
+		push(S, str[i]);
+	}
+	for (int j = 0; str[j]!='\0'; i++)
+	{
+		pop(S, e);
+		if (*e!=str[j])
+		{
+			Destory_stack(S);
+			return false;
+		}
+
+	}
+}
+
+
